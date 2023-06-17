70811a280aefdc2d2f8c38c6622e06085b534865
Dev <hydrochain@gmail.com>
2023-06-17 20:50:03 +0700

    jil

--- bot1.js
+++ bot1.js
@@ -1,30 +1,48 @@
 const axios = require("axios");
-let url = "https://paxunitas.com/assets/api/api.php";
+let email = [
+  "zonawallets@gmail.com",
+  "hydrochaintoken@gmail.com",
+  "kcuan35@gmail.com",
+  "zonachaon@gmail.com",
+  "ziddanalfarizy26@gmail.com",
+  "eulisyuniarti2203@gmail.com",
+  "lesmanaanjing@gmail.com",
+  "acipsyaputraj@gmail.com",
+  "iisyamhuri@gmail.com",
+  "ziddanalfarizy12@gmail.com",
+];
 
-let json = {
-  email: "zonawallets@gmail.com",
-  func: "verifyUser",
-  wallet: "0x4bcabe941ba359a93926bd5b1555887d419573a4",
-};
+let address = "0x4bcabe941ba359a93926bd5b1555887d419573a4";
+
+async function claim(email) {
+  try {
+    let url = "https://paxunitas.com/assets/api/api.php";
+    let response = await axios.post(url, {
+      func: "verifyUser",
+      wallet: address,
+      email: email,
+    });
+
+    if (response.data != null) {
+      console.log(
+        "\u001b[33m" +
+          email +
+          "\u001b[0m: \u001b[31m" +
+          response.data.msg +
+          "\u001b[0m"
+      );
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 async function main() {
-  const res = await axios.post("https://paxunitas.com/assets/api/api.php", {
-    func: "verifyUser",
-    wallet: "0x4bcabe941ba359a93926bd5b1555887d419573a4",
-    email: "kcuan35@gmail.com",
-  });
-  const res2 = await axios.post("https://paxunitas.com/assets/api/api.php", {
-    func: "verifyUser",
-    wallet: "0x4bcabe941ba359a93926bd5b1555887d419573a4",
-    email: "zonawallets@gmail.com",
-  });
-  if (res.data === null) {
-    await main();
-  } else {
-    console.log(res.data);
-    console.log(res2.data);
-    await main();
+  for (let i = 0; i < email.length; i++) {
+    await claim(email[i]);
   }
+
+  await main();
 }
 
 main();
